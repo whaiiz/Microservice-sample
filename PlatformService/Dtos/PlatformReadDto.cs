139d5ca04948d9447fdfbcaf396fdf0d51d64c97
Jaime <jaime_reis18@outlook.pt>
2023-05-01 20:34:29 +0000

    Creation of models and mapping configuraiton

---
+++ PlatformService/Dtos/PlatformReadDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Dtos
+{
+    public class PlatformReadDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Publisher { get; set; } = string.Empty;
+
+        public string Cost { get; set; } = string.Empty;
+    }
+}
