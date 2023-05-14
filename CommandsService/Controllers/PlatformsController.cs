4bad698d5f67776185504dc7fab81397269f58e0
Jaime <jaime_reis18@outlook.pt>
2023-05-14 07:43:24 +0000

    Creation of new endpoitns for platforms and commands

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using CommandsService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace CommandsService.Controllers
 {
@@ -6,10 +11,26 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
-        {
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
 
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
         }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Platform>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting platforms");
+
+            var platformItems = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+        }
+
 
         [HttpPost]
         public ActionResult TestInboundConnection()
