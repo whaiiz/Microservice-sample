91801cdd273b5a861f699d8f471731e1110d21d4
Jaime <jaime_reis18@outlook.pt>
2023-05-14 07:41:52 +0000

    CommnadService -> Creation of repos and Dtos

---
+++ CommandsService/Dtos/CommandCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CommandsService.Dtos
+{
+    public class CommandCreateDto
+    {
+        public string HowTo { get; set; } = string.Empty;
+
+        public string CommandLine { get; set; } = string.Empty;
+    }
+}
---
+++ CommandsService/Dtos/CommandReadDto.cs
@@ -0,0 +1,16 @@
+﻿using CommandsService.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Dtos
+{
+    public class CommandReadDto
+    {
+        public int Id { get; set; }
+
+        public string HowTo { get; set; } = string.Empty;
+
+        public string CommandLine { get; set; } = string.Empty;
+
+        public int PlatformId { get; set; }
+    }
+}
